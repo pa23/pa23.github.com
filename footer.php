4d26e4abac6b15f35c063bfdc1f4a901db1cf9c6
Artem Petrov <pa2311@gmail.com>
2011-05-26 17:50:39 +0400

    github.com does not allow use PHP??? Unfortunately.

--- footer.php
+++
@@ -1,9 +0,0 @@
-<P STYLE="border-top: none; border-bottom: 1px solid #000000; border-left: none; border-right: none; padding-top: 0cm; padding-bottom: 0.08cm; padding-left: 0cm; padding-right: 0cm"><BR></P>
-<P ALIGN=CENTER>
-    <A HREF="https://github.com/pa23">github</A> | 
-    <A HREF="http://vkontakte.ru/pa2311">vkontakte</A> | 
-    <A HREF="http://www.facebook.com/profile.php?id=100001046779586">facebook</A>
-</P>
-</BODY>
-</HTML>
-
